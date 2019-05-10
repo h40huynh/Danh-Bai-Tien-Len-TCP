ad333725deb2ffd3ff3ea111927e6e3e5af3b08a
h40huynh <17520444@gm.uit.edu.vn>
2019-05-10 15:11:37 +0700

    Create class

--- Server/ServerMainForm.Designer.cs
+++ Server/ServerMainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Server
 {
-    partial class Form1
+    partial class ServerMainForm
     {
         /// <summary>
         /// Required designer variable.
--- Server/ServerMainForm.cs
+++ Server/ServerMainForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Server
 {
-    public partial class Form1 : Form
+    public partial class ServerMainForm : Form
     {
-        public Form1()
+        public ServerMainForm()
         {
             InitializeComponent();
         }
