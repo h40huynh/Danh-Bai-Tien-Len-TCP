197e36dd200a6874526f5602601cd9cfc6cf7b44
h40huynh <17520444@gm.uit.edu.vn>
2019-05-10 19:25:15 +0700

    Some function of Room

--- Server/Room.cs
+++ Server/Room.cs
@@ -8,5 +8,37 @@ namespace Server
 {
     public class Room
     {
+        private int id;
+        private int money;
+        private Player[] players;
+
+        private int winnerPosition;
+        private int numberOfPlayer;
+
+        public Room(int money)
+        {
+            this.money = money;
+
+            players = new Player[4];
+        }
+
+        public void addPlayer(Player p)
+        {
+            for(int i = 0; i < 4; i++)
+            {
+                if(players[i] == null)
+                {
+                    players[i] = p;
+                    numberOfPlayer++;
+                    return;
+                }
+            }
+        }
+
+        public void deletePlayer(Player p)
+        {
+            int position = Array.FindIndex(players, player => player == p);
+            players[position] = null;
+        }
     }
 }
