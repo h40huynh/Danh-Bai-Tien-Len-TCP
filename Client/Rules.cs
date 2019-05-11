7ef96b960396fc76b282f1f0118b869b29786b7f
Huy3nMy <50505066+Huy3nMy@users.noreply.github.com>
2019-05-11 16:48:48 +0700

    add check current

--- Client/Rules.cs
+++ Client/Rules.cs
@@ -10,15 +10,22 @@ namespace Client
     {
         int[] enamyCard;
         int[] myCard;
-        public Rules(string curentCard, string dataReceive)
+        int[] currentCard; // bài đang chọn
+
+        public Rules(string currentCard1)
         {
-            string[] myCardstr = curentCard.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            string[] currCardstr = currentCard1.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            currentCard = Array.ConvertAll<string, int>(currCardstr, int.Parse);
+
+        }
+        public Rules(string myyCard, string dataReceive)
+        {
+            string[] myCardstr = myyCard.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
             string[] enamyCardstr = dataReceive.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries);
             myCard = Array.ConvertAll<string, int>(myCardstr, int.Parse);
             enamyCard = Array.ConvertAll<string, int>(enamyCardstr, int.Parse);
-
         }
-        public bool check()
+        public bool check() // check ban đầu
         {
             // đánh lẻ 
             if (enamyCard.Length == 1)
@@ -26,11 +33,11 @@ namespace Client
                 // đánh lẻ =2
                 if (enamyCard[0] / 10 == 15)
                 {
-                    if (checksingle() == true || checkdoublegroup(3) == true || checkquadra() == true)
+                    if (checksingle(myCard) == true || checkdoublegroup(myCard,3) == true || checkquadra(myCard) == true)
                         return true;
                 }
                 else
-                    return checksingle();
+                    return checksingle(myCard);
             }
 
             // đánh đôi
@@ -39,52 +46,52 @@ namespace Client
                 // đánh đôi 2
                 if (enamyCard[0] / 10 == 15)
                 {
-                    if (checkdouble() == true || checkquadra() == true || checkdoublegroup(4) == true)
+                    if (checkdouble(myCard) == true || checkquadra(myCard) == true || checkdoublegroup(myCard,4) == true)
                         return true;
                 }
                 else
-                    return checkdouble();
+                    return checkdouble(myCard);
             }
             // đaanhs 3
-            if (enamyCard.Length == 3 && enamyCard[0] == enamyCard[1])
+            if (enamyCard.Length == 3 && enamyCard[0]/10 == enamyCard[1]/10)
             {
-                return checktriple();
+                return checktriple(myCard);
             }
             // đánh tứ quý
-            if (enamyCard.Length == 4 && enamyCard[0] == enamyCard[3])
+            if (enamyCard.Length == 4 && enamyCard[0]/10 == enamyCard[3]/10)
             {
-                return checkquadra();
+                return checkquadra(myCard);
             }
             // đánh đôi thông
-            if (enamyCard.Length >= 6 && enamyCard[0] == enamyCard[1])
+            if (enamyCard.Length >= 6 && enamyCard[0]/10 == enamyCard[1]/10)
             {
-                return checkdoublegroup(enamyCard.Length / 2);
+                return checkdoublegroup(myCard, enamyCard.Length / 2);
             }
             // còn lại là đánh sảnh
-            return checkgroup();
+            return checkgroup(myCard);
         }
 
-        private bool checksingle() // đánh lẻ
+        private bool checksingle(int[] curr) // đánh lẻ
         {
             // mycard max của mình > enamycard
-            if (myCard[myCard.Length - 1] > enamyCard[0])
+            if (curr[curr.Length - 1] > enamyCard[0])
                 return true;
             else
                 return false;
         }
 
-        private bool checkdouble() // đánh đôi
+        private bool checkdouble(int[] curr) // đánh đôi
         {
             // mycard max < enamycard max => false
-            if (myCard[myCard.Length - 1] < enamyCard[1])
+            if (curr[curr.Length - 1] < enamyCard[1])
                 return false;
             // tìm đôi lớn nhất 
-            for (int i = myCard.Length - 1; i > 0; i--)
+            for (int i = curr.Length - 1; i > 0; i--)
             {
-                // có đôi và mycard lớn của đôi lớn hơn cardmax của địch
-                if (myCard[i] > enamyCard[1])
+                // có đôi và curr lớn của đôi lớn hơn cardmax của địch
+                if (curr[i] > enamyCard[1])
                 {
-                    if (myCard[i] / 10 == myCard[i - 1] / 10)
+                    if (curr[i] / 10 == curr[i - 1] / 10)
                         return true;
                 }
                 else
@@ -94,18 +101,18 @@ namespace Client
             return false;
         }
 
-        private bool checktriple() // đánh ba
+        private bool checktriple(int[] curr) // đánh ba
         {
-            // mycard max < enamycard max => false
-            if (myCard[myCard.Length - 1] < enamyCard[2])
+            // curr max < enamycard max => false
+            if (curr[curr.Length - 1] < enamyCard[2])
                 return false;
             // tìm ba lớn nhất 
-            for (int i = myCard.Length - 1; i > 1; i--)
+            for (int i = curr.Length - 1; i > 1; i--)
             {
-                // có ba và mycard lớn của ba lớn hơn cardmax của địch
-                if (myCard[i] > enamyCard[2])
+                // có ba và curr lớn của ba lớn hơn cardmax của địch
+                if (curr[i] > enamyCard[2])
                 {
-                    if (myCard[i] / 10 == myCard[i - 1] / 10 && myCard[i] / 10 == myCard[i - 2])
+                    if (curr[i] / 10 == curr[i - 1] / 10 && curr[i] / 10 == curr[i - 2]/10)
                         return true;
                     else
                         return false;
@@ -114,18 +121,18 @@ namespace Client
             return false;
         }
 
-        private bool checkquadra()// tứ quý
+        private bool checkquadra(int[] curr)// tứ quý
         {
-            // mycard max < enamycard min => false
-            if (myCard[myCard.Length - 1] < enamyCard[0])
+            // curr max < enamycard min => false
+            if (curr[curr.Length - 1] < enamyCard[0])
                 return false;
             // tìm tứ quý lớn nhất 
-            for (int i = myCard.Length - 1; i > 2; i--)
+            for (int i = curr.Length - 1; i > 2; i--)
             {
-                // có tứ quý và mycard lớn của tứ quý lớn hơn cardmax của địch
-                if (myCard[i] > enamyCard[0])
+                // có tứ quý và curr lớn của tứ quý lớn hơn cardmax của địch
+                if (curr[i] > enamyCard[0])
                 {
-                    if (myCard[i] / 10 == myCard[i - 1] / 10 && myCard[i] / 10 == myCard[i - 2] && myCard[i] / 10 == myCard[i - 3])
+                    if (curr[i] / 10 == curr[i - 1] / 10 && curr[i] / 10 == curr[i - 2] / 10 && curr[i] / 10 == curr[i - 3] / 10)
                         return true;
                 }
                 else
@@ -134,66 +141,219 @@ namespace Client
             return false;
         }
 
-        private bool checkgroup() // check sảnh
+        private bool checkgroup(int[] curr) // check sảnh
         {
             //chuỗi số khác nhau
             List<int> arrayIncreased = new List<int>();
-            arrayIncreased.Add(myCard[0]); 
-            for (int i = 1; i < myCard.Length - 1; i++)
+            arrayIncreased.Add(curr[0]); 
+            for (int i = 1; i < curr.Length - 1; i++)
             {
-                if (myCard[i] / 10 == 15)
+                if (curr[i] / 10 == 15)
                     break;
-                if (myCard[i] > myCard[i - 1])
-                    arrayIncreased.Add(myCard[i]);
+                if (curr[i] > curr[i - 1])
+                    arrayIncreased.Add(curr[i]);
             }
 
 
-            // mycard max < enamycard min => false
-            if (myCard[myCard.Length - 1] < enamyCard[enamyCard.Length - 1])
+            // curr max < enamycard min => false
+            if (curr[curr.Length - 1] < enamyCard[enamyCard.Length - 1])
                 return false;
 
             int dem = 1;
             for (int i = arrayIncreased.Count - 1; i > 0; i--)
             {
-                if (arrayIncreased[i] == arrayIncreased[i - 1] + 1)
+                if (arrayIncreased[i] / 10 == arrayIncreased[i - 1] / 10 + 1)
                     dem++;
                 else
                     dem = 1;
-                if (dem == enamyCard.Length && myCard[i + dem - 2] > enamyCard[enamyCard.Length - 1])
+                if (dem == enamyCard.Length && curr[i + dem - 2] > enamyCard[enamyCard.Length - 1])
                     return true;
             }
             return false;
         }
 
-        private bool checkdoublegroup(int len)
+        private bool checkdoublegroup(int[] curr,int len)
         {
             //mảng các đôi
             List<int> arrdouble = new List<int>();
-            for (int i = myCard.Length - 1; i > 0; i--)
+            for (int i = curr.Length - 1; i > 0; i--)
             {
-                if (myCard[i] == myCard[i - 1])
+                if (curr[i]/10 == curr[i - 1]/10)
                 {
-                    arrdouble.Add(myCard[i]);
-                    arrdouble.Add(myCard[i - 1]);
+                    arrdouble.Add(curr[i]);
+                    arrdouble.Add(curr[i - 1]);
                     i--;
                 }
             }
-            // mycard max < enamycard max => false
+            // curr max < enamycard max => false
             if (arrdouble[arrdouble.Count - 1] < enamyCard[enamyCard.Length - 1])
                 return false;
 
             int dem = 1;
             for (int i = arrdouble.Count - 1; i > 2; i = i - 2)
             {
-                if (arrdouble[i] == arrdouble[i - 2] + 1)
+                if (arrdouble[i]/10 == arrdouble[i - 2]/10 + 1)
                     dem++;
                 else
                     dem = 1;
-                if (dem * 2 == enamyCard.Length && myCard[i + (len - 2) * 2] > enamyCard[enamyCard.Length - 1])
+                if (dem * 2 == enamyCard.Length && curr[i + (len - 2) * 2] > enamyCard[enamyCard.Length - 1])
                     return true;
             }
             return false;
 
+        }
+
+        private bool issingle()
+        {
+            if (currentCard.Length == 1)
+                return true;
+            else
+                return false;
+        }
+        private bool isdouble()
+        {
+            if (currentCard.Length == 2 && currentCard[0]/10 == currentCard[1]/10)
+                return true;
+            else
+                return false;
+        }
+
+        private bool istriple()
+        {
+            if (currentCard.Length == 3 && currentCard[0]/10 == currentCard[1]/10 && currentCard[1]/10 == currentCard[2]/10)
+                return true;
+            else
+                return false;
+        }
+
+        private bool isquadra()
+        {
+            if (currentCard.Length == 4 && currentCard[0]/10 == currentCard[1]/10 && currentCard[1]/10 == currentCard[2]/10 && currentCard[2]/10 == currentCard[3]/10)
+                return true;
+            else
+                return false;
+        }
+
+        private bool isgroup()// sanhr
+        {
+            if(currentCard.Length >=3)
+            {
+                for(int i =0; i<currentCard.Length-1; i++)
+                {
+                    if (currentCard[i] / 10 + 1 != currentCard[i + 1] / 10)
+                        return false;
+                }
+                return true;
+            }
+            return false;
+        }
+
+        private bool isdoublegroup(int len )
+        {
+            if (currentCard.Length == len * 2)
+            {
+                for (int i = 0; i < currentCard.Length - 2; i = i + 2)
+                {
+                    if (currentCard[i] / 10 + 1 != currentCard[i + 2] / 10)
+                        return false;
+                } 
+                return true;
+            }
+            return false;
+        }
+
+        // kiểm tra sau khi chọn bài để đánh có hợp lệ không
+        private bool checkcurrent()
+        {
+            // ban đầu enamyCard = null ( bạn thư)
+            // currentcard là chuỗi không giảm (bạn Hào)
+            if(enamyCard.Length == 0) // người thắng đánh đầu tiên
+            {
+                if (issingle() == true || isdouble() == true || istriple() == true || isquadra() == true || isgroup() == true || isdoublegroup(3) == true || isdoublegroup(4) == true || isdoublegroup(5) == true)
+                {
+                    return true;
+                }
+                else
+                    return false;
+            }
+            else
+            {
+                // đánh lẻ 
+                if (enamyCard.Length == 1)
+                {
+                    // đánh lẻ =2
+                    if (enamyCard[0] / 10 == 15)
+                    {
+                        if (checksingle(currentCard) && issingle())
+                            return true;
+                        else if (checkdoublegroup(currentCard, 3) && isdoublegroup(3))
+                            return true;
+                        else if (checkquadra(currentCard) == true && isquadra())
+                            return true;
+                        else
+                            return false;
+                    }
+                    else
+                    {
+                        if (checksingle(currentCard) && issingle())
+                            return true;
+                        else
+                            return false;
+                    }
+                }
+
+                // đánh đôi
+                if (enamyCard.Length == 2)
+                {
+                    // đánh đôi 2
+                    if (enamyCard[0] / 10 == 15)
+                    {
+                        if (checkdouble(currentCard) && isdouble())
+                            return true;
+                        else if (checkdoublegroup(currentCard, 4) && isdoublegroup(3))
+                            return true;
+                        else if (checkquadra(currentCard) == true && isquadra())
+                            return true;
+                        else
+                            return false;
+                    }
+                    else
+                    {
+                        if (checkdouble(currentCard) && isdouble())
+                            return true;
+                        else
+                            return false;
+                    }
+                }
+                // đaanhs 3
+                if (enamyCard.Length == 3 && enamyCard[0] / 10 == enamyCard[1] / 10)
+                {
+                    if (checktriple(currentCard) && istriple())
+                        return true;
+                    else
+                        return false;
+                }
+                // đánh tứ quý
+                if (enamyCard.Length == 4 && enamyCard[0] / 10 == enamyCard[3] / 10)
+                {
+                    if (checkquadra(currentCard) && isquadra())
+                        return true;
+                    else
+                        return false;
+                }
+                // đánh đôi thông
+                if (enamyCard.Length >= 6 && enamyCard[0] / 10 == enamyCard[1] / 10)
+                {
+                    if (checkdoublegroup(currentCard, enamyCard.Length/2) && isdoublegroup(enamyCard.Length/2))
+                        return true;
+                    else
+                        return false;
+                }
+                // còn lại là đánh sảnh
+                if (isgroup() && checkgroup(currentCard))
+                    return true;
+                return false;
+            }
         }
 
     }
